f647c093d06f5e431cda4ab3f2e7fa1bc8ea7cff
rizkyilhampra <rizkyilhamp16@gmail.com>
2023-02-12 16:10:37 +0800

    modified login and layout view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.custom')
 
+@section('title', 'Login')
 @section('content')
     <div class="row">
         <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
@@ -13,14 +14,17 @@
                 </div>
 
                 <div class="card-body">
-                    <form method="POST" action="#" class="needs-validation" novalidate="">
+                    <form method="POST" action="{{ route('login') }}" class="needs-validation" novalidate="">
+                        @csrf
                         <div class="form-group">
                             <label for="email">Email</label>
-                            <input id="email" type="email" class="form-control" name="email" tabindex="1" required
-                                autofocus>
-                            <div class="invalid-feedback">
-                                Please fill in your email
-                            </div>
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                                value="{{ old('email') }}" name="email" tabindex="1" autofocus>
+                            @error('email')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                         </div>
 
                         <div class="form-group">
@@ -32,11 +36,13 @@
                                     </a>
                                 </div>
                             </div>
-                            <input id="password" type="password" class="form-control" name="password" tabindex="2"
-                                required>
-                            <div class="invalid-feedback">
-                                please fill in your password
-                            </div>
+                            <input id="password" type="password"
+                                class="form-control @error('password') is-invalid @enderror" name="password" tabindex="2">
+                            @error('password')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                         </div>
                         <div class="form-group">
                             <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
--- resources/views/layouts/custom.blade.php
+++ resources/views/layouts/custom.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-    <title>Blank Page &mdash; Stisla</title>
+    <title>@yield('title', 'Custom')</title>
 
     <!-- General CSS Files -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
