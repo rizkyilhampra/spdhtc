b397d755b1deae01d81f0bb6acbc503ddabe8cc0
rizkyilhampra <rizkyilhamp16@gmail.com>
2023-02-24 16:47:16 +0800

    get avatar from googleAuth

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,4 +1,5 @@
 <form class="form-inline mr-auto">
+
     <ul class="navbar-nav mr-3">
         <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
         <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i class="fas fa-search"></i></a>
@@ -198,9 +199,10 @@
             </div>
         </div>
     </li>
+
     <li class="dropdown"><a href="#" data-toggle="dropdown"
             class="nav-link dropdown-toggle nav-link-lg nav-link-user">
-            <img alt="image" src="../assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
+            <img alt="image" src="{{ auth()->user()->getAvatarAttribute() }}" class="rounded-circle mr-1">
             <div class="d-sm-none d-lg-inline-block">Hi, {{ auth()->user()->name }}</div>
         </a>
         <div class="dropdown-menu dropdown-menu-right">
@@ -224,6 +226,7 @@
         </div>
     </li>
 </ul>
+
 
 @section('jsCustom')
     <script>
