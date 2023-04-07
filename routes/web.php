1d381d0d7af45561a521b6c449a9e53f51b87178
rizkyilhampra <rizkyilhamp16@gmail.com>
2023-04-07 08:24:44 +0800

    fix reference controllers was problem

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Http\Controllers\Controller;
 use App\Http\Controllers\SocialAuthController;
 use App\Http\Controllers\UserController;
-use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,19 +23,19 @@ Route::get('/', [UserController::class, 'index']);
 // });
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('home', [App\Http\Controllers\Controller::class, 'authenticated'])->name('home');
+    Route::get('home', [\App\Http\Controllers\Controller::class, 'authenticated'])->name('home');
 
     Route::prefix('admin')->middleware('can:asAdmin')->group(function () {
-        Route::get('beranda', [App\Http\Controllers\AdminController::class, 'beranda'])->name('admin.beranda');
-        Route::get('histori-diagnosis', [App\Http\Controllers\AdminController::class, 'diagnosis'])->name('admin.diagnosis');
-        Route::get('penyakit', [App\Http\Controllers\AdminController::class, 'penyakit'])->name('admin.penyakit');
-        Route::get('gejala', [App\Http\Controllers\AdminController::class, 'gejala'])->name('admin.gejala');
-        Route::get('rule', [App\Http\Controllers\AdminController::class, 'rule'])->name('admin.rule');
+        Route::get('beranda', [\App\Http\Controllers\AdminController::class, 'beranda'])->name('admin.beranda');
+        Route::get('histori-diagnosis', [\App\Http\Controllers\AdminController::class, 'diagnosis'])->name('admin.diagnosis');
+        Route::get('penyakit', [\App\Http\Controllers\AdminController::class, 'penyakit'])->name('admin.penyakit');
+        Route::get('gejala', [\App\Http\Controllers\AdminController::class, 'gejala'])->name('admin.gejala');
+        Route::get('rule', [\App\Http\Controllers\AdminController::class, 'rule'])->name('admin.rule');
     });
 
     Route::prefix('user')->middleware('can:asUser')->group(function () {
-        Route::get('edit-profile', [App\Http\Controllers\UserProfileController::class, 'index'])->name('edit-profile');
-        Route::get('edit-profile/lokasi/kota/{id}', [App\Http\Controllers\KotaProvinsiController::class, 'indexCity'])->name('kota');
+        Route::get('edit-profile', [\App\Http\Controllers\UserProfileController::class, 'index'])->name('edit-profile');
+        Route::get('edit-profile/lokasi/kota/{id}', [\App\Http\Controllers\KotaProvinsiController::class, 'indexCity'])->name('kota');
     });
 
     Route::get('home-user', function () {
