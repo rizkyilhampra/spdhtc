7644204fbd35d2ce9b0e3457c19e4d1a49e15fa0
rizkyilhampra <rizkyilhamp16@gmail.com>
2023-04-10 05:20:02 +0800

    minor fix

--- resources/views/user/diagnosis.blade.php
+++ resources/views/user/diagnosis.blade.php
@@ -1,14 +1,18 @@
 @extends('layouts.user.app')
 @section('content')
     <div id="diagnosis" class="row min-vh-100 align-content-center section">
-        Content
         {{-- ISI CONTENT DISINI --}}
+
+        Content
+
+        {{-- END CONTENT --}}
     </div>
 @endsection
 
 @push('scriptPerPage')
     <script type="text/javascript">
         $(document).ready(function() {
+            applyNavbarClassesDark();
             $('.diagnosis').addClass('active');
             let navLink = document.querySelector('ul.navbar-nav').querySelectorAll('li a');
             for (let i = 0; i < navLink.length; i++) {
