b03656467b6bdd86af2f8201541f897efa77a5b5
rizkyilhampra <rizkyilhamp16@gmail.com>
2023-04-24 18:57:26 +0800

    create table diagnosa and add eloquent

--- app/Models/User.php
+++ app/Models/User.php
@@ -64,4 +64,9 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return $this->hasOne(UserProfile::class, 'user_id');
     }
+
+    public function diagnosa()
+    {
+        return $this->hasMany(Diagnosa::class);
+    }
 }
