1653cde2090b5e29ba63ed06ba44c42ccff20d6e
rizkyilhampra <rizkyilhamp16@gmail.com>
2023-05-22 04:02:52 +0800

    add github api for show collaborators

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
         integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
     <!-- CSS Libraries -->
-    @stack('cssLibraries');
+    @stack('cssLibraries')
 
     <!-- Template CSS -->
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
@@ -53,7 +53,7 @@
     <script src="{{ asset('assets/js/stisla.js') }}"></script>
 
     <!-- JS Libraies -->
-    @stack('jsLibraries');
+    @stack('jsLibraries')
 
     <!-- Template JS File -->
     <script src="{{ asset('assets/js/scripts.js') }}"></script>
