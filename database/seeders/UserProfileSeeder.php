81bba366fdc096f02f7184cec596b1c3e3299c67
rizkyilhampra <rizkyilhamp16@gmail.com>
2023-05-24 04:55:00 +0800

    change seeder user profile

--- database/seeders/UserProfileSeeder.php
+++ database/seeders/UserProfileSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\UserProfile;
 use Faker\Factory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,7 +17,7 @@ class UserProfileSeeder extends Seeder
      */
     public function run()
     {
-        $profession = ['Developer', 'Designer', 'Manager', 'Architect'];
+        $profession = ['Petani', 'Lainnya'];
 
         for ($i = 1; $i <= User::count(); $i++) {
             \App\Models\UserProfile::create([
@@ -24,8 +25,17 @@ class UserProfileSeeder extends Seeder
                 'address' => Factory::create()->address,
                 'city' => 'Anytown',
                 'province' => 'BC',
-                'profession' => $profession[rand(0, 3)],
+                'profession' => $profession[rand(0, 1)],
             ]);
         }
+
+        UserProfile::where('user_id', 12)->update([
+            'province' => 13,
+            'city' => 144
+        ]);
+        UserProfile::where('user_id', 13)->update([
+            'province' => 13,
+            'city' => 144
+        ]);
     }
 }
