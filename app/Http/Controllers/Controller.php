ab07d541e80849038493487fed89294d26d651eb
rizkyilhampra <rizkyilhamp16@gmail.com>
2023-05-24 05:34:26 +0800

    change loginduration condition

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,11 +7,8 @@ use Carbon\Carbon;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Http\Request as Request;
 use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate as FacadesGate;
-use Laravel\Fortify\Contracts\LoginResponse as LoginResponseContract;
 use Laravel\Fortify\Fortify;
 
 class Controller extends BaseController
@@ -34,6 +31,15 @@ class Controller extends BaseController
     public function LoginDuration()
     {
         $lastLogin = auth()->user()->last_login_at;
-        return $loginDuration = Carbon::parse($lastLogin)->diffInMinutes();
+        $loginDuration = Carbon::parse($lastLogin)->diffInMinutes();
+        if ($loginDuration < 60) {
+            $loginDuration = $loginDuration . ' menit';
+        } else if ($loginDuration >= 60 && $loginDuration < 1440) {
+            $loginDuration = Carbon::parse($lastLogin)->diffInHours() . ' jam';
+        } else if ($loginDuration >= 1440) {
+            $loginDuration = Carbon::parse($lastLogin)->diffInDays() . ' hari';
+        }
+
+        return $loginDuration;
     }
 }
