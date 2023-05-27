eadf1ba86e0d98fb2b031c67d9cc66874f7ba5d9
rizkyilhampra <rizkyilhamp16@gmail.com>
2023-05-27 19:06:47 +0800

    refactor and add splash screen

--- resources/views/layouts/user/app.blade.php
+++ resources/views/layouts/user/app.blade.php
@@ -6,15 +6,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{ asset('/spesified-assets/splash-screen.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('/spesified-assets/user/style.css') }}">
     <script src="https://kit.fontawesome.com/06b8a1e79b.js" type="text/javascript" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.css">
+    <link rel="stylesheet" href="{{ asset('/spesified-assets/aos.css') }}" />
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
     @stack('stylePerPage')
 </head>
 
 <body class="bg-light-subtle">
+    <div class="splash-screen">
+        <div class="la-timer la-dark la-3x">
+            <div>
+            </div>
+        </div>
+    </div>
     <main>
         <section>
             @include('layouts.user.navbar')
@@ -27,6 +37,7 @@
     <div class="rounded-circle" id="upScroll">
         <i class="fas fa-chevron-up fa-lg"></i>
     </div>
+
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
         integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
     </script>
@@ -35,51 +46,16 @@
     </script>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"
         integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
+
     <script src="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.js"></script>
-
-    <script type="text/javascript">
-        function applyNavbarClassesDark() {
-            return $('.navbar')
-                .removeClass('bg-body-transparent')
-                .addClass('color-fren-green')
-                .attr('data-bs-theme', 'dark').css({
-                    'transition': 'all .5s ease-in-out'
-                }),
-                $('svg path')
-                .attr('style', 'fill: #fff !important')
-                .css({
-                    'transition': 'all .5s ease-in-out'
-                }),
-                $('.navbar-nav li a div button').removeClass('btn-outline-dark').addClass('btn-outline-light').css({
-                    'transition': 'all .5s ease-in-out'
-                })
-        }
-
-        function applyNavbarClassesLight() {
-            return $('.navbar')
-                .removeClass('color-fren-green')
-                .removeAttr('data-bs-theme')
-                .addClass('bg-body-transparent').css({
-                    'transition': 'all .5s ease-in-out'
-                }),
-                $('svg path')
-                .removeAttr('style')
-                .css({
-                    'transition': 'all .5s ease-in-out'
-                }),
-                $('.navbar-nav li a div button').removeClass('btn-outline-light').addClass('btn-outline-dark').css({
-                    'transition': 'all .5s ease-in-out'
-                })
-        }
-        const notyf = new Notyf({
-            position: {
-                x: 'right',
-                y: 'top',
-            },
-            dismissible: true,
-        });
+    <script src="https://cdn.jsdelivr.net/npm/simple-parallax-js@5.5.1/dist/simpleParallax.min.js" type="text/javascript">
     </script>
+    <script src="{{ asset('/spesified-assets/aos.js') }}" type="text/javascript"></script>
+    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
+
     @stack('scriptPerPage')
+    <script src="{{ asset('/spesified-assets/user/script.js') }}"></script>
 </body>
 
 </html>
