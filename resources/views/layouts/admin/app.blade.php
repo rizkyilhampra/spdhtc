1f2ca6adf4f389004d1ad22e95fb8c3736b93b1f
rizkyilhampra <rizkyilhamp16@gmail.com>
2023-06-01 01:39:12 +0800

    remove unused file

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -58,7 +58,6 @@
 
     <!-- Template JS File -->
     <script src="{{ asset('assets/js/scripts.js') }}"></script>
-    <script src="{{ asset('assets/js/custom.js') }}"></script>
 
     <!-- Page Specific JS File -->
     @stack('jsCustom')
