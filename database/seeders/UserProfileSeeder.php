8744eb98f9f0d36fc75ad1f6fa9d38043acd05eb
rizkyilhampra <rizkyilhamp16@gmail.com>
2023-06-28 17:39:12 +0800

    change user profile with model

--- database/seeders/UserProfileSeeder.php
+++ database/seeders/UserProfileSeeder.php
@@ -19,14 +19,29 @@ class UserProfileSeeder extends Seeder
     {
         $profession = ['Petani', 'Lainnya'];
 
-        for ($i = 1; $i <= User::count(); $i++) {
-            \App\Models\UserProfile::create([
-                'user_id' => $i,
-                'address' => Factory::create()->address,
-                'city' => 144,
-                'province' => 13,
-                'profession' => $profession[rand(0, 1)],
-            ]);
-        }
+        /**
+         * Create Custom User Profile with Factory
+         * Uncomment if you want to use User Seeder with Factory
+         */
+        // for ($i = 1; $i <= User::count(); $i++) {
+        //     \App\Models\UserProfile::create([
+        //         'user_id' => $i,
+        //         'address' => Factory::create()->address,
+        //         'city' => 144,
+        //         'province' => 13,
+        //         'profession' => $profession[rand(0, 1)],
+        //     ]);
+        // }
+
+        /**
+         * Create Custom User Profile with Model
+         */
+        UserProfile::create([
+            'user_id' => 1,
+            'address' => '',
+            'city' => 144,
+            'province' => 13,
+            'profession' => $profession[rand(0, 1)],
+        ]);
     }
 }
