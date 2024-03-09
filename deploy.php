aadda72a82e40e53483fb78731657b4ec479fdfe
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-03-09 15:15:05 +0800

    ci: update workflows

--- deploy.php
+++ deploy.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Deployer;
 
 require 'recipe/laravel.php';
@@ -7,15 +8,10 @@ require 'recipe/laravel.php';
 
 set('repository', 'git@github.com:rizkyilhampra/spdhtc.git');
 
-add('shared_files', ['.env']);
-add('shared_dirs', ['storage']);
-add('writable_dirs', [
-    'bootstrap/cache',
-    'storage',
-    'storage/app',
-    'storage/framework',
-    'storage/logs',
-]);
+// it will be override by laravel recipe. See https://deployer.org/docs/7.x/recipe/laravel
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
 
 // Hosts
 
@@ -23,6 +19,25 @@ host('spdhtc.rizkyilhampra.me')
     ->set('hostname', '209.97.169.80')
     ->set('remote_user', 'deployer')
     ->set('deploy_path', '~/htdocs/spdhtc.rizkyilhampra.me');
+
+// Tasks
+
+task('deploy:secrets', function () {
+    file_put_contents(__DIR__.'/.env', getenv('DOT_ENV'));
+    upload('.env', get('deploy_path').'/shared');
+});
+
+task('deploy', [
+    'deploy:prepare',
+    'deploy:secrets',
+    'deploy:vendors',
+    'artisan:storage:link',
+    'artisan:config:cache',
+    'artisan:route:cache',
+    'artisan:view:cache',
+    'artisan:event:cache',
+    'deploy:publish',
+]);
 
 // Hooks
 
