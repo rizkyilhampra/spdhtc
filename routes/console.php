e611e38112e79475555697d52c955432924cf70e
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-11-17 11:18:17 +0800

    chore(caching): adds commands to recache provinces and cities data

--- routes/console.php
+++ routes/console.php
@@ -1,19 +1,15 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use App\Console\Commands\RecachingCities;
+use App\Console\Commands\RecachingProvinces;
+use Illuminate\Support\Facades\Schedule;
 
-/*
-|--------------------------------------------------------------------------
-| Console Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of your Closure based console
-| commands. Each Closure is bound to a command instance allowing a
-| simple approach to interacting with each command's IO methods.
-|
-*/
+Schedule::command(RecachingProvinces::class)
+    ->daily()
+    ->timezone('Asia/Makassar')
+    ->runInBackground();
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Schedule::command(RecachingCities::class)
+    ->daily()
+    ->timezone('Asia/Makassar')
+    ->runInBackground();
