c17b3d31e67aad465af29d79100ba7b3485d9ef9
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-11-21 06:41:56 +0800

    fix(console): updates caching logic for provinces and cities

--- app/Console/Commands/RecachingCities.php
+++ app/Console/Commands/RecachingCities.php
@@ -2,32 +2,24 @@
 
 namespace App\Console\Commands;
 
-use App\Http\Controllers\Admin\BerandaController;
+use App\Http\Controllers\KotaProvinsiController;
+use App\Models\UserProfile;
 use Illuminate\Console\Command;
 
 class RecachingCities extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'cache:cities';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Recache cities data for chart from RajaOngkir API to local cache';
 
-    /**
-     * Execute the console command.
-     */
     public function handle()
     {
-        (new BerandaController())->chartCity();
+        $provinces = UserProfile::select('province')->groupBy('province')->get();
 
-        $this->info('Cities data has been recached!');
+        $request = new \Illuminate\Http\Request;
+
+        foreach ($provinces as $province) {
+            (new KotaProvinsiController)->indexCity($request, $province);
+        }
     }
 }
