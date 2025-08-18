3d76389d73ad3c6370f19a9e340100a3d8ac6683
rizkyilhampra <rizkyilhamp16@gmail.com>
2025-08-18 22:43:01 +0800

    fix(user): update select2 CDN link
    Updates the Select2 CDN link to the latest release candidate version to ensure compatibility and access to the newest features.

--- resources/views/user/user.blade.php
+++ resources/views/user/user.blade.php
@@ -141,7 +141,7 @@
         @include('user.detail-diagnosis-modal')
         @push('styleLibraries')
             <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css">
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" />
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css">
             <link rel="stylesheet"
                 href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" />
         @endpush
